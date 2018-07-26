8237111a4b141bdf1e7fbe04272a2b98fc92e6f0
developer <boris-plus@mail.ru>
2018-07-26 17:52:52 +0300

    get_functions_names_in_trees - обобщенная логика get_top_verbs_in_path и get_top_functions_names_in_path

--- 001_2018_07_25/dclnt.py
+++ 001_2018_07_25/dclnt.py
@@ -84,23 +84,41 @@ def get_all_words_in_path(path):
     return flat([split_snake_case_name_to_words(function_name) for function_name in function_names])
 
 
+def get_functions_names_in_trees(trees):
+    function_names = [
+        f for f in flat(
+            [
+                [
+                    node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)
+                ] for t in trees
+            ]
+        ) if not (f.startswith('__') and f.endswith('__'))
+    ]
+    return function_names
+
+
 def get_top_verbs_in_path(path, top_size=10):
     global Path
     Path = path
     trees = [t for t in get_trees(None) if t]
-    fncs = [f for f in
-            flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]) if
-            not (f.startswith('__') and f.endswith('__'))]
+    # fncs = [f for f in
+    #         flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]) if
+    #         not (f.startswith('__') and f.endswith('__'))]
+    fncs = get_functions_names_in_trees(trees)
     print('functions extracted')
     verbs = flat([get_verbs_from_function_name(function_name) for function_name in fncs])
     return collections.Counter(verbs).most_common(top_size)
 
 
 def get_top_functions_names_in_path(path, top_size=10):
+    # t = get_trees(path)
+    # nms = [f for f in
+    #        flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if
+    #        not (f.startswith('__') and f.endswith('__'))]
+    # return collections.Counter(nms).most_common(top_size)
+
     t = get_trees(path)
-    nms = [f for f in
-           flat([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in t]) if
-           not (f.startswith('__') and f.endswith('__'))]
+    nms = get_functions_names_in_trees(t)
     return collections.Counter(nms).most_common(top_size)
 
 
