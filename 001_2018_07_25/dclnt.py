87a6972cb318a7e3df33bc8db2b7af9aed07bb25
developer <boris-plus@mail.ru>
2018-07-26 16:00:43 +0300

    общее в split_snake_case_name_to_words and get_verbs_from_function_name

--- 001_2018_07_25/dclnt.py
+++ 001_2018_07_25/dclnt.py
@@ -17,8 +17,24 @@ def is_verb(_word):
     return pos_info[0][1] == 'VB'
 
 
+def filter_function_raw(x):
+    return x
+
+
+def filtered_split(name, sep=None, filter_function=None):
+    if sep is None:
+        sep = '_'
+    if filter_function is None:
+        filter_function = filter_function_raw
+    return [_part for _part in name.split(sep) if filter_function(_part)]
+
+
+def split_snake_case_name_to_words(name):
+    return filtered_split(name)
+
+
 def get_verbs_from_function_name(function_name):
-    return [_word for _word in function_name.split('_') if is_verb(_word)]
+    return filtered_split(function_name, filter_function=is_verb)
 
 
 def get_all_names(_tree):
@@ -32,7 +48,7 @@ def get_trees(_path=None, with_file_names=None, with_file_content=None, top_coun
     if _path is None:
         _path = ''
     if top_count is None:
-        top_count = 0
+        top_count = -1
     if with_file_names is None:
         with_file_names = False
     if with_file_content is None:
@@ -44,7 +60,7 @@ def get_trees(_path=None, with_file_names=None, with_file_content=None, top_coun
         for file in files:
             if file.endswith('.py'):
                 file_names.append(os.path.join(dir_name, file))
-                if len(file_names) == top_count:
+                if len(file_names) == top_count and top_count >= 0:
                     break
     print('total %s files' % len(file_names))
     for file_name in file_names:
@@ -66,13 +82,14 @@ def get_trees(_path=None, with_file_names=None, with_file_content=None, top_coun
     return trees
 
 
-def get_all_words_in_path(path):
-    trees = [t for t in get_trees(path) if t]
-    function_names = [f for f in flat([get_all_names(t) for t in trees]) if
-                      not (f.startswith('__') and f.endswith('__'))]
 
-    def split_snake_case_name_to_words(name):
-        return [n for n in name.split('_') if n]
+
+def get_all_words_in_path(_path):
+    trees = get_trees(_path)
+    function_names = [
+        f for f in flat([get_all_names(t) for t in trees]) if
+        not (f.startswith('__') and f.endswith('__'))
+    ]
 
     return flat([split_snake_case_name_to_words(function_name) for function_name in function_names])
 
